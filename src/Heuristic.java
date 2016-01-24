a849bbccbebc9da8d2a020ad502833b5b48788c0
alonso_martinez <almartnz@gmail.com>
2016-01-23 19:43:17 -0500

    Heuristic

--- src/Heuristic.java
+++ src/Heuristic.java
@@ -12,8 +12,8 @@ public class Heuristic {
 		int temp = 0, temp2 = 0;
 		System.out.println("");
 
-		for(int i = 0; i < 3;i++){
-			for(int j = 0; j < 3; j++){
+		for(int i = 0; i < 6;i++){
+			for(int j = 0; j < 7; j++){
 				System.out.print(currentState.board[i][j]+" ");
 			}
 			System.out.println("");
